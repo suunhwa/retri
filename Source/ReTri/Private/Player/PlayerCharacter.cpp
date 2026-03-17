a67790dbeddba845ae469b8bd2ab83f1bda293d0
suunhwa <snna628@gmail.com>
2026-03-17 17:45:57 +0900

    feat: add player movement, camera, and input binding

---
+++ Source/ReTri/Private/Player/PlayerCharacter.cpp
@@ -0,0 +1,105 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/PlayerCharacter.h"
+
+#include "GameFramework/SpringArmComponent.h"
+#include "InputAction.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+
+// Sets default values
+APlayerCharacter::APlayerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> CharacterMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/ParagonRevenant/Characters/Heroes/Revenant/Meshes/Revenant.Revenant'"));
+	if (CharacterMesh.Succeeded())
+	{
+		GetMesh()->SetSkeletalMesh(CharacterMesh.Object);
+		GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -90.0f), FRotator(0.0f, -90.0f, 0.0f));
+	}
+	
+	ConstructorHelpers::FClassFinder<UAnimInstance> AnimBP(TEXT("/Script/Engine.AnimBlueprint'/Game/Player/Animations/ABP_Player.ABP_Player_C'")); 
+	if (AnimBP.Succeeded())
+	{
+		GetMesh()->SetAnimInstanceClass(AnimBP.Class);
+	}
+	
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	SpringArmComp->SetupAttachment(RootComponent);
+	SpringArmComp->SetRelativeLocation(FVector(0.0f, 0.0f, 60.0f));
+	SpringArmComp->SetRelativeRotation(FRotator(-60.f, 0.f, 0.f));
+	SpringArmComp->TargetArmLength = 1800.f;   
+	SpringArmComp->bUsePawnControlRotation = false;
+	SpringArmComp->bDoCollisionTest = false;  
+	
+	CamComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CamComp"));
+	CamComp->SetupAttachment(SpringArmComp);
+	CamComp->SetFieldOfView(60.f);
+	
+	ConstructorHelpers::FObjectFinder<UInputAction> TempMoveInput(TEXT("/Script/EnhancedInput.InputAction'/Game/Player/Inputs/IA_Move.IA_Move'"));
+	if (TempMoveInput.Succeeded())
+	{
+		ia_Move = TempMoveInput.Object;
+	}
+	
+	ConstructorHelpers::FObjectFinder<UInputMappingContext> TempIMC(TEXT("/Script/EnhancedInput.InputMappingContext'/Game/Player/Inputs/IMC_Player.IMC_Player'"));
+	if (TempIMC.Succeeded())
+	{
+		imc_Player = TempIMC.Object;
+	}
+}
+
+// Called when the game starts or when spawned
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	/*FVector p0 = GetActorLocation();
+	FVector vt = direction * speed * DeltaTime;
+	SetActorLocation(p0 + vt);
+	direction = FVector::ZeroVector;*/
+}
+
+// Called to bind functionality to input
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// 입력 바인딩
+	auto pc = Cast<APlayerController>(Controller);
+	if (pc)
+	{
+		auto subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(pc->GetLocalPlayer());
+		if (subsystem)
+		{
+			subsystem->AddMappingContext(imc_Player, 0);
+		}
+		
+		auto playerInput = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+		if (playerInput)
+		{
+			playerInput->BindAction(ia_Move, ETriggerEvent::Triggered, this, &APlayerCharacter::OnMove);
+		}
+	}
+}
+
+void APlayerCharacter::OnMove(const struct FInputActionValue& inputValue)
+{
+	FVector2D value = inputValue.Get<FVector2D>();
+	AddMovementInput(FVector::ForwardVector, value.X);
+	AddMovementInput(FVector::RightVector, value.Y);
+	/*direction.X = value.X;
+	direction.Y = value.Y;*/
+}
+
