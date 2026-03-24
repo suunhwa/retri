ee79922b6fd7e9694a0fb513cdf7794db742500e
suunhwa <snna628@gmail.com>
2026-03-24 15:41:48 +0900

    fix: uproject syntax and include order errors

--- Source/ReTri/Private/Level/Actors/NZW_TestPlayer.cpp
+++ Source/ReTri/Private/Level/Actors/NZW_TestPlayer.cpp
@@ -1,8 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
-#include "ReTriGameData.h"
 #include "Level/Actors/NZW_TestPlayer.h"
+#include "ReTriGameData.h"
 #include "Level/Actors/InteractableBase.h"
 
 #include "Kismet/KismetSystemLibrary.h"
