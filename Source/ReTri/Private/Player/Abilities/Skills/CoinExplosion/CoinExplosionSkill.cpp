d46245cc2fa68c205ca9b2143e125a51ce505afe
sh <snna628@gmail.com>
2026-04-08 06:14:30 +0900

    feat: add CoinExplosion skill
    - CoinProjectile: 첫 적 명중 시 AoE 폭발 + 1초 기절
    - CoinExplosionSkill: 골드 1% 소모, 폭발 후 50/50 배율 누적/리셋
    - DT_PlayerSkills: CoinExplosion 행 추가
    ---
    - CoinProjectile: AoE explosion on first enemy hit with 1s stun
    - CoinExplosionSkill: costs 1% gold, 50/50 damage multiplier on explode
    - DT_PlayerSkills: add CoinExplosion row

---
+++ Source/ReTri/Private/Player/Abilities/Skills/CoinExplosion/CoinExplosionSkill.cpp
@@ -0,0 +1,93 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Player/Abilities/Skills/CoinExplosion/CoinExplosionSkill.h"
+
+#include "GameFramework/Character.h"
+#include "Kismet/GameplayStatics.h"
+#include "Math/UnrealMathUtility.h"
+#include "Player/PlayerCharacter.h"
+#include "Player/ReTriPlayerController.h"
+#include "Player/Components/StatComponent.h"
+#include "Player/Projectiles/CoinProjectile.h"
+
+UCoinExplosionSkill::UCoinExplosionSkill()
+{
+	Cooldown = 5.f;
+}
+
+bool UCoinExplosionSkill::CanActivate(ACharacter* Owner)
+{
+	if (!Owner) return false;
+
+	APlayerCharacter* Player = Cast<APlayerCharacter>(Owner);
+	if (!Player) return false;
+
+	UStatComponent* StatComp = Player->GetStatComponent();
+	if (!StatComp) return false;
+
+	// 던질 동전이 없으면 사용 불가
+	return StatComp->GetGold() > 0;
+}
+
+void UCoinExplosionSkill::Activate(ACharacter* Owner)
+{
+	if (!Owner) return;
+	if (!CoinProjectileClass) return;
+
+	APlayerCharacter* Player = Cast<APlayerCharacter>(Owner);
+	if (!Player) return;
+
+	UStatComponent* StatComp = Player->GetStatComponent();
+	if (!StatComp) return;
+
+	AReTriPlayerController* PC = Cast<AReTriPlayerController>(Owner->GetController());
+	if (!PC) return;
+
+	// 현재 골드의 1% (최소 1)
+	const int32 CurrentGold = StatComp->GetGold();
+	const int32 GoldCost = FMath::Max(1, FMath::CeilToInt(CurrentGold * 0.01f));
+
+	if (!StatComp->SpendGold(GoldCost)) return;
+
+	// 피해량 = 소모 골드 × 현재 배율
+	const float FinalDamage = GoldCost * ExplosionDamageCoeff;
+
+	FVector MouseWorldPos;
+	if (!PC->GetMouseWorldPosition(MouseWorldPos)) return;
+
+	const FVector OwnerLoc = Owner->GetActorLocation();
+	const FVector LaunchDir = (MouseWorldPos - OwnerLoc).GetSafeNormal2D();
+
+	const FTransform SpawnTransform(LaunchDir.Rotation(), OwnerLoc);
+
+	ACoinProjectile* Coin = Owner->GetWorld()->SpawnActorDeferred<ACoinProjectile>(
+		CoinProjectileClass,
+		SpawnTransform,
+		Owner,
+		Owner,
+		ESpawnActorCollisionHandlingMethod::AlwaysSpawn
+	);
+
+	if (Coin)
+	{
+		// 폭발 완료 콜백 등록
+		FOnCoinExploded Callback;
+		Callback.BindUObject(this, &UCoinExplosionSkill::RollExplosionMultiplier);
+
+		Coin->Init(FinalDamage, Owner->GetController(), Callback);
+		UGameplayStatics::FinishSpawningActor(Coin, SpawnTransform);
+	}
+}
+
+void UCoinExplosionSkill::RollExplosionMultiplier()
+{
+	// 50% 확률로 배율 2배, 50% 확률로 기본값 리셋
+	if (FMath::RandBool())
+	{
+		ExplosionDamageCoeff *= 2.f;
+	}
+	else
+	{
+		ExplosionDamageCoeff = BaseExplosionDamageCoeff;
+	}
+}
---
+++ Source/ReTri/Private/Player/Projectiles/CoinProjectile.cpp
@@ -0,0 +1,167 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Player/Projectiles/CoinProjectile.h"
+
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Enemy/EnemyBase.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "NiagaraFunctionLibrary.h"
+#include "TimerManager.h"
+
+ACoinProjectile::ACoinProjectile()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionComp"));
+	CollisionComp->SetSphereRadius(12.f);
+	CollisionComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	CollisionComp->SetCollisionObjectType(ECC_GameTraceChannel5);
+	CollisionComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	CollisionComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	CollisionComp->SetGenerateOverlapEvents(true);
+	RootComponent = CollisionComp;
+
+	BodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyMesh"));
+	BodyMesh->SetupAttachment(CollisionComp);
+	BodyMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	MoveComp = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("MoveComp"));
+	MoveComp->SetUpdatedComponent(CollisionComp);
+	MoveComp->InitialSpeed = 1400.f;
+	MoveComp->MaxSpeed = 1400.f;
+	MoveComp->bRotationFollowsVelocity = true;
+	MoveComp->ProjectileGravityScale = 0.f;
+
+	InitialLifeSpan = 4.f;
+}
+
+void ACoinProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CollisionComp->OnComponentBeginOverlap.AddDynamic(this, &ACoinProjectile::OnOverlap);
+
+	if (FlightSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(GetWorld(), FlightSound, GetActorLocation());
+	}
+}
+
+void ACoinProjectile::Init(float InDamage, AController* InInstigator, FOnCoinExploded InCallback)
+{
+	Damage = InDamage;
+	InstigatorController = InInstigator;
+	OnCoinExploded = InCallback;
+}
+
+void ACoinProjectile::OnOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+	bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (bHasExploded) return;
+	if (!OtherActor || OtherActor == GetOwner()) return;
+
+	// 적에만 반응
+	AEnemyBase* Enemy = Cast<AEnemyBase>(OtherActor);
+	if (!Enemy) return;
+
+	bHasExploded = true;
+	MoveComp->StopMovementImmediately();
+
+	Explode(GetActorLocation());
+}
+
+void ACoinProjectile::Explode(FVector ExplosionLocation)
+{
+	// VFX
+	if (ExplosionEffect)
+	{
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+			GetWorld(), ExplosionEffect, ExplosionLocation,
+			FRotator::ZeroRotator, FVector(1.f), true, true,
+			ENCPoolMethod::None, true
+		);
+	}
+
+	// SFX
+	if (ExplosionSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(GetWorld(), ExplosionSound, ExplosionLocation);
+	}
+
+	// AoE 범위 내 적 탐색
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+	ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECC_Pawn));
+
+	TArray<AActor*> IgnoreActors;
+	IgnoreActors.Add(GetOwner());
+
+	TArray<AActor*> HitActors;
+	UKismetSystemLibrary::SphereOverlapActors(
+		GetWorld(), ExplosionLocation, ExplosionRadius,
+		ObjectTypes, AEnemyBase::StaticClass(),
+		IgnoreActors, HitActors
+	);
+
+	for (AActor* Actor : HitActors)
+	{
+		if (!Actor) continue;
+
+		// 빛 피해 적용
+		UGameplayStatics::ApplyDamage(
+			Actor, Damage,
+			InstigatorController.Get(), this,
+			UDamageType::StaticClass()
+		);
+
+		// 1초 기절: 이동 비활성화
+		if (ACharacter* EnemyChar = Cast<ACharacter>(Actor))
+		{
+			if (UCharacterMovementComponent* EnemyMoveComp = EnemyChar->GetCharacterMovement())
+			{
+				EnemyMoveComp->DisableMovement();
+			}
+
+			// AI 일시 정지
+			if (AController* EnemyController = EnemyChar->GetController())
+			{
+				EnemyController->StopMovement();
+			}
+
+			// 기절 해제 타이머
+			FTimerHandle StunTimer;
+			TWeakObjectPtr<AActor> WeakActor(Actor);
+			GetWorld()->GetTimerManager().SetTimer(StunTimer, [this, WeakActor]()
+			{
+				UnstunEnemy(WeakActor);
+			}, StunDuration, false);
+		}
+	}
+
+	// 폭발 완료 → 스킬에 50/50 판정 요청
+	OnCoinExploded.ExecuteIfBound();
+
+	Destroy();
+}
+
+void ACoinProjectile::UnstunEnemy(TWeakObjectPtr<AActor> WeakEnemy)
+{
+	if (!WeakEnemy.IsValid()) return;
+
+	ACharacter* EnemyChar = Cast<ACharacter>(WeakEnemy.Get());
+	if (!EnemyChar) return;
+
+	if (AEnemyBase* Enemy = Cast<AEnemyBase>(EnemyChar))
+	{
+		if (Enemy->bIsDead) return;
+	}
+
+	if (UCharacterMovementComponent* EnemyMoveComp = EnemyChar->GetCharacterMovement())
+	{
+		EnemyMoveComp->SetMovementMode(MOVE_Walking);
+	}
+}
